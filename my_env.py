00b7965e6921b3bf0637838c1955d7e1f814b2be
Anton Shabat <anton.shabat@gmail.com>
2025-08-27 19:29:45 +0300

    removed env file from tracking

--- my_env.py
+++
@@ -1,3 +0,0 @@
-API_KEY = "92fd7846eb9c4a18ae18e65e8906ff84"
-NEON_APP_PASSWORD = "npg_fRUeYcyEI37g"
-NEON_CONNECTION_STRING = "postgresql://event_planner_app:npg_fRUeYcyEI37g@ep-billowing-silence-a2t3w1ix-pooler.eu-central-1.aws.neon.tech/event_planner?sslmode=require&channel_binding=require"
