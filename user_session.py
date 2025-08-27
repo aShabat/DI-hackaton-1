f1c6b90ab88fdec01c4fdfe9ae0ee7c2f7b5637a
Anton Shabat <anton.shabat@gmail.com>
2025-08-27 23:38:05 +0300

    .

--- user_session.py
+++ user_session.py
@@ -4,9 +4,32 @@ from neon import NeonConnection
 
 
 class UserSession:
-    def __init__(self, user_id: UUID) -> None:
-        self.__conn = NeonConnection()
+    def __init__(
+        self,
+        user_id: UUID,
+        country_code: None | str = None,
+        connection: NeonConnection | None = None,
+    ) -> None:
+        if connection is None:
+            connection = NeonConnection()
+        self.__conn = connection
         self.__id = user_id
+        self.country_code = country_code
+
+    def view_event(self, title: str):
+        event = self.__conn.execute(
+            """
+            select event_date, event_start, event_end, description
+            from events
+            where user_id = %s
+            and title = %s
+            """,
+            (self.__id, title),
+        )
+        if len(event) == 0:
+            return None
+
+        return event[0]
 
     def check_datetime(self, time_to_check: datetime):
         day = time_to_check.date()
@@ -58,7 +81,7 @@ class UserSession:
         )
         return len(result) > 0
 
-    def remove_event(self, day: date, title: str):
+    def remove_event(self, title: str):
         result = self.__conn.execute(
             """
             delete from events
@@ -66,6 +89,6 @@ class UserSession:
             and title = %s
             returning event_id;
             """,
-            (day, title),
+            (self.__id, title),
         )
         return len(result) > 0
